f46e56cd0412304a76c3411d9133e33a64d0415d
Zeke <zekenaulty>
2024-09-15 13:30:42 -0500

    beta dom evaluator

--- src/domEvaluator.js
+++ src/domEvaluator.js
@@ -1,10 +1,33 @@
 // domEvaluator.js
 
-export class DomEvaluator {
-    constructor(data = {}) {
-      this.data = data;
+class DomEvaluator {
+    constructor() {
+      if (DomEvaluator.instance) {
+        return DomEvaluator.instance;
+      }
+  
+      this.data = {};
+      this.isDirty = false;
+      this.updateQueue = [];
+      this.isUpdating = false;
+      DomEvaluator.instance = this;
     }
   
+    // Factory method to get the singleton instance
+    static getInstance() {
+      if (!DomEvaluator.instance) {
+        DomEvaluator.instance = new DomEvaluator();
+      }
+      return DomEvaluator.instance;
+    }
+  
+    // Method to set the data object and make it reactive
+    setData(data) {
+      this.data = this.makeReactive(data);
+      this.evaluate(); // Initial evaluation
+    }
+  
+    // Method to evaluate the DOM
     evaluate(element = document.body) {
       const walker = document.createTreeWalker(
         element,
@@ -31,6 +54,7 @@ export class DomEvaluator {
       });
     }
   
+    // Parse text nodes and replace expressions
     parseText(text) {
       const regex = /{([^{}]+)}/g;
       let match;
@@ -45,6 +69,7 @@ export class DomEvaluator {
       return result;
     }
   
+    // Resolve expressions using the data object
     resolveExpression(expression) {
       try {
         // Only allow property access on the data object
@@ -56,8 +81,65 @@ export class DomEvaluator {
       }
     }
   
-    setData(newData) {
-      this.data = newData;
+    // Make data object reactive using Proxy
+    makeReactive(obj) {
+      const self = this;
+  
+      function createReactive(target) {
+        return new Proxy(target, {
+          get(target, prop, receiver) {
+            const value = Reflect.get(target, prop, receiver);
+            if (typeof value === 'object' && value !== null) {
+              return createReactive(value); // Recursive for nested objects
+            }
+            return value;
+          },
+          set(target, prop, value, receiver) {
+            const oldValue = target[prop];
+            const result = Reflect.set(target, prop, value, receiver);
+            if (oldValue !== value) {
+              self.markDirty();
+            }
+            return result;
+          },
+        });
+      }
+  
+      return createReactive(obj);
+    }
+  
+    // Mark the evaluator as dirty and queue an update
+    markDirty() {
+      if (!this.isDirty) {
+        this.isDirty = true;
+        this.queueUpdate();
+      }
+    }
+  
+    // Queue an update using setTimeout
+    queueUpdate() {
+      if (!this.isUpdating) {
+        this.isUpdating = true;
+        setTimeout(() => {
+          this.digest();
+        }, 0);
+      }
+    }
+  
+    // Perform the DOM update
+    digest() {
+      if (this.isDirty) {
+        this.evaluate();
+        this.isDirty = false;
+      }
+      this.isUpdating = false;
+    }
+  
+    // Manual method to trigger an update
+    update() {
+      this.markDirty();
     }
   }
+  
+  export { DomEvaluator };
   
